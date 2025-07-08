f6847f44a36140a17b211387920345c6cd6bd60d
Manasa <m.neruganti@gmail.com>
2025-07-08 16:13:57 +0530

    Added home route, started adding add contact route

--- app.py
+++ app.py
@@ -3,9 +3,39 @@ from flask import Flask, render_template, request, redirect, url_for
 from models import db, Contact
 
 app = Flask(__name__)
+
+# defined where the database is -> uses a SQLite file called contacts.db in this folder
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///contacts.db'
+
+# this tracks objects changes and sends signals, so we disable it to reduce wait times
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# we can initialize the database here and connect the DB with the app
 db.init_app(app)
 
+# app_context() gives info about the app to create_all()
+with app.app_context(): 
+    
+    # This tells SQLAlchemy to create tables based on 'Contact' class (the model)
+    db.create_all()
+
+# Defines a route to the homepage
 @app.route('/')
+def index():
+    
+    # Fetches all saved contacts from the database and stores it
+    contacts = Contact.query.all()
+    
+    # Send these contacts to html file
+    return render_template('index.html', contacts=contacts)
+
+# Defines another route to add a contact. This accepts GET, which displays the form,
+# and POST, which processes the form information
+app.route('/add', methods=['GET', 'POST'])
+def add_contact():
+    if request.method() == 'POST':
+        name = request.form['name']
+        phone = request.form['phone']
+        email = request.form['email']  
+    
 
--- models.py
+++ models.py
@@ -1,9 +1,17 @@
 from flask_sqlalchemy import SQLAlchemy
 
+# initialize the database as an object of SQlAlchemy
 db = SQLAlchemy()
 
+
+# Create a new contact class, taking in the database model as a parameter
 class Contact(db.Model):
+    
+    # assign an ID in the system, making it the primary key and an int
     id = db.Column(db.Integer, primary_key=True)
+    
+    # assign a name, phone, and email strng with certain amount of characters and 
+    # make sure that the column cannot contain NULL values
     name = db.Column(db.String(100), nullable=False)
     phone = db.Column(db.String(15), nullable=False)
     email = db.Column(db.String(100), nullable=False)
